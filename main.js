dbcc734e4584cba63fc04db17141df4a7a81b04b
aldodenegri <aldodenegri00@gmail.com>
2025-10-30 10:55:07 +0100

    Aggiunta outputReverse(100-0) e modifiche via bootstrap

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 let textJS = document.getElementById("textJS");
 let output = "";
-for(let i=1;i<=100;i++)//in alterantiiva posso usare anche for(let i=1;i<101;i++)
-    {
-        
-        //controllo se il numero è multiplo di 3 e 5 tramite l'operatore modulo %, il resto della divisione
-        if(i%3==0 && i%5==0)//avviene prima la condizione più restrittiva
-        {
-            output="fizzbuzz";
-            console.log(output);
-        }
-        else if(i%3==0)
-        {
-            output="fizz";
-            console.log(output);
-        }
-        else if(i%5==0)
-        {
-            output="buzz";
-            console.log(output);
-        }
-        else {
-            output=i;
-            console.log(output);
-        }
-        //stampo nell'html aggiungendo l'ultimo valore calcolato a capo ad ogni iterazione
-        textJS.innerHTML += output + "<br>";
-    }
-    
-    
+let outputReverse = "";
+textJS.innerHTML = ""; //svuoto il contenuto html prima di iniziare il ciclo
+for (let i = 1, j = 100; i <= 100 && j >= 0; i++, j--)//in alterantiiva posso usare anche for(let i=1;i<101;i++)
+{
+    //la logica dei cotrolli per i e per j devono essere separate altrimenti si sovrascrivono a vicenda e non ottengo il controllo desierato
+    //controllo se il numero è multiplo di 3 e 5 tramite l'operatore modulo %, il resto della divisione sia per i che per j
+    if (i % 3 == 0 && i % 5 == 0) output = "fizzbuzz";
+    else if (i % 3 == 0) output = "fizz";
+    else if (i % 5 == 0) output = "buzz";
+    else output = i;
+
+    if (j % 3 == 0 && j % 5 == 0) outputReverse = "fizzbuzz";
+    else if (j % 3 == 0) outputReverse = "fizz";
+    else if (j % 5 == 0) outputReverse = "buzz";
+    else outputReverse = j;
+    //stampo nell'html aggiungendo l'ultimo valore calcolato a capo ad ogni iterazione
+    textJS.innerHTML +=
+        `<div class="row">
+            <div class="col-6">
+                <span>${output}</span>
+            </div>`
+        +
+        `<div class="col-6 d-flex justify-content-end">
+                <span>${outputReverse}</span>
+            </div>
+        </div>`;
+    console.log(output + " - " + outputReverse);
+}
+
