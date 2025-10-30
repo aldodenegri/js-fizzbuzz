8d605e1cb2b6e89f71eb32b3b98d659a55e6c834
aldodenegri <aldodenegri00@gmail.com>
2025-10-30 10:14:31 +0100

    esercizio completo con stampa sia nella pagina che nella console

---
+++ main.js
@@ -0,0 +1,30 @@
+let textJS = document.getElementById("textJS");
+let output = "";
+for(let i=0;i<101;i++)//101 numeri in totale, voglio vedere sia lo 0 che il 100
+    {
+        
+        //controllo se il numero è multiplo di 3 e 5 tramite l'operatore modulo %, il resto della divisione
+        if(i%3==0 && i%5==0)//avviene prima la condizione più restrittiva
+        {
+            output="fizzbuzz";
+            console.log(output);
+        }
+        else if(i%3==0)
+        {
+            output="fizz";
+            console.log(output);
+        }
+        else if(i%5==0)
+        {
+            output="buzz";
+            console.log(output);
+        }
+        else {
+            output=i;
+            console.log(output);
+        }
+        //stampo nell'html aggiungendo l'ultimo valore calcolato a capo ad ogni iterazione
+        textJS.innerHTML += output + "<br>";
+    }
+    
+    
